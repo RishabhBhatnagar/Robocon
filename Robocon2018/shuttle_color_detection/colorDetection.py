07c7ef570c9dae8306445b093e8a756a2c15e294
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-02-19 14:31:48 +0530

    Code Refined

--- Robocon2018/shuttle_color_detection/colorDetection.py
+++ Robocon2018/shuttle_color_detection/colorDetection.py
@@ -1,55 +1,39 @@
 import cv2
 import numpy as np
 import os
-
 cap = cv2.VideoCapture(1)
 
+def threshImg(img, lower, upper, color = "some_color", showImg = 1):
+    lower_color = np.array(lower)
+    upper_color = np.array(upper)
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    if showImg : cv2.imshow(color, mask)
 
 i = 0
-while(1):
-    _, frame = cap.read()
+while cap.isOpened():
+    ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
-
-
-    lower_golden = np.array([50,0,0])
-    upper_golden = np.array([80,150,150])    
-    mask = cv2.inRange(hsv, lower_golden, upper_golden)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    img = cv2.imread('wiki.jpg',0)
-    cv2.imshow("yellow_mask", mask)
-
     
-    lower_blue = np.array([100,50,30])
-    upper_blue = np.array([120,255,255])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    #counting number of black Pixels:
-    number_of_blue_pixels = cv2.countNonZero(mask)
-    cv2.imshow('blue_mask',mask)
-
-    lower_red = np.array([30,150,100])
-    upper_red = np.array([255,255,180])
+    "Writing images to folder : "
+    cv2.imwrite(os.path.join("TestImages" , "image" + str(i) + ".png"), frame)
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    #GOLDEN
+    threshImg(frame.copy(), [50, 0, 0], [80, 150, 150], "Golden", 1)
 
-    #counting number of black Pixels:
-    number_of_red_pixels = cv2.countNonZero(mask)
+    #BLUE    
+    threshImg(frame.copy(), [100, 50, 30], [120, 255, 255], "Blue", 1)
 
-
-    if number_of_red_pixels > number_of_blue_pixels:print("red")
-    else:print("blue")
-    
-    cv2.imshow('frame',frame)
-    cv2.imshow('red_mask',mask)
-    cv2.imshow('res',res)
-
-    cv2.imwrite(os.path.join("TestImages", "test_image" + str(i) + ".png"), frame)
-    
+    #RED:
+    threshImg(frame.copy(), [30, 150, 100], [255, 255, 180], "Red", 1)
+    if ret == True:
+        
+        cv2.imshow('frame',frame)
     i += 1
+    print(i)
+
+    if cv2.waitKey(1) == 27:
+        break
 
 cv2.destroyAllWindows()
 cap.release()
@@ -59,7 +43,6 @@ cap.release()
 RED:
     lower_red = np.array([30,150,100])
     upper_red = np.array([255,255,180])
-
 BLUE:
     lower_blue = np.array([100,50,30])
     upper_blue = np.array([120,255,255])

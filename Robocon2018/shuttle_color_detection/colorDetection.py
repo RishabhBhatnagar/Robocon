9187066d6c825c553b01097a6e3a879cc9cf3511
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-02-19 12:55:03 +0530

    Test Images stored

--- Robocon2018/shuttle_color_detection/colorDetection.py
+++ Robocon2018/shuttle_color_detection/colorDetection.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
+import os
 
 cap = cv2.VideoCapture(1)
+
 
 i = 0
 while(1):
@@ -12,8 +14,7 @@ while(1):
 
 
     lower_golden = np.array([50,0,0])
-    upper_golden = np.array([80,150,150])
-    
+    upper_golden = np.array([80,150,150])    
     mask = cv2.inRange(hsv, lower_golden, upper_golden)
     res = cv2.bitwise_and(frame,frame, mask= mask)
     img = cv2.imread('wiki.jpg',0)
@@ -22,16 +23,12 @@ while(1):
     
     lower_blue = np.array([100,50,30])
     upper_blue = np.array([120,255,255])
-    
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     #counting number of black Pixels:
     number_of_blue_pixels = cv2.countNonZero(mask)
-
     cv2.imshow('blue_mask',mask)
-
-
 
     lower_red = np.array([30,150,100])
     upper_red = np.array([255,255,180])
@@ -45,14 +42,13 @@ while(1):
 
     if number_of_red_pixels > number_of_blue_pixels:print("red")
     else:print("blue")
-
-    cv2.imwrite("image" + str(i) + ".png", frame)
+    
     cv2.imshow('frame',frame)
     cv2.imshow('red_mask',mask)
     cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+
+    cv2.imwrite(os.path.join("TestImages", "test_image" + str(i) + ".png"), frame)
+    
     i += 1
 
 cv2.destroyAllWindows()

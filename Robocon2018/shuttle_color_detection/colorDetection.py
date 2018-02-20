bd6f58b50c21fd7c61b7fd5f170c688643c6d9a9
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-02-20 19:41:33 +0530

    cool ranges

--- Robocon2018/shuttle_color_detection/colorDetection.py
+++ Robocon2018/shuttle_color_detection/colorDetection.py
@@ -8,32 +8,33 @@ def threshImg(img, lower, upper, color = "some_color", showImg = 1):
     upper_color = np.array(upper)
     mask = cv2.inRange(hsv, lower_color, upper_color)
     res = cv2.bitwise_and(frame,frame, mask= mask)
-    if showImg : cv2.imshow(color, mask)
+    if showImg : cv2.imshow(color, res)
+    return mask
 
-i = 0
-while cap.isOpened():
-    ret, frame = cap.read()
+i = 21000
+while True:
+    _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     "Writing images to folder : "
     cv2.imwrite(os.path.join("TestImages" , "image" + str(i) + ".png"), frame)
+
+    show = [1, 1, 1, 0]
     
     #GOLDEN
-    threshImg(frame.copy(), [50, 0, 0], [80, 150, 150], "Golden", 1)
+    golden_img = threshImg(frame, [10, 40, 120], [35, 240, 250], "Golden", show[0])
 
-    #BLUE    
-    threshImg(frame.copy(), [100, 50, 30], [120, 255, 255], "Blue", 1)
+    #BLUE    Not sure ranges.
+    blue_img = threshImg(frame, [20, 10, 25],
+                                [255, 150, 155], "Blue", show[1])
 
-    #RED:
-    threshImg(frame.copy(), [30, 150, 100], [255, 255, 180], "Red", 1)
-    if ret == True:
-        
-        cv2.imshow('frame',frame)
-    i += 1
-    print(i)
-
+    #RED: 
+    red_img = threshImg(frame, [150,150,50],
+                               [180, 255, 255], "Red", show[2])
+    
+    cv2.imshow("frame", frame)
     if cv2.waitKey(1) == 27:
-        break
+        pass
 
 cv2.destroyAllWindows()
 cap.release()
@@ -41,13 +42,12 @@ cap.release()
 
 """
 RED:
-    lower_red = np.array([30,150,100])
-    upper_red = np.array([255,255,180])
+    lower_red = np.array([150,150,50])
+    upper_red = np.array([180, 255, 255])
 BLUE:
     lower_blue = np.array([100,50,30])
     upper_blue = np.array([120,255,255])
-GOLDEN:
-    lower_golden = np.array([50,0,0])
-    upper_golden = np.array([80,150,150])
-    
+
+Not Blue and Golden :
+[(0,0), (0,10 : 160,255), (0, 24 : 221, 255)]
 """

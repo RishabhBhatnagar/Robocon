764fd343336fdb69e795ed92e52452320c780f13
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-02-24 12:29:06 +0530

    with arduino output

--- Robocon2018/shuttle_color_detection/colorDetection.py
+++ Robocon2018/shuttle_color_detection/colorDetection.py
@@ -1,18 +1,7 @@
 import cv2
 import numpy as np
 import os
-import RPi.GPIO as GPIO
-
-cap = cv2.VideoCapture(0)
-
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(3,GPIO.OUT)
-GPIO.setup(5,GPIO.OUT)
-GPIO.setup(7,GPIO.OUT)
-
+cap = cv2.VideoCapture(1)
 
 def threshImg(img, lower, upper, color = "some_color", showImg = 1):
     lower_color = np.array(lower)
@@ -22,14 +11,9 @@ def threshImg(img, lower, upper, color = "some_color", showImg = 1):
     if showImg : cv2.imshow(color, res)
     return mask
 
-def rpi_output(pin, *args):
-    GPIO.output(pin, GPIO.HIGH)
-    for pins in args :
-        GPIO.output(pins, GPIO.LOW)
-
 number_of_frames = 10
 colors = {"red":0, "blue":0, "green":0}
-while cap.isOpened and number_of_frames:
+while cap.isOpened():
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
@@ -54,27 +38,20 @@ while cap.isOpened and number_of_frames:
     
     dominant_color = max(r, g, b)
 
-    if dominant_color == r :
-        print("temp_red")
-        colors['red'] = colors.get('red') + 1
-    if dominant_color == g :
-        print("temp_golden")
-        colors['golden'] = colors.get('golden') + 1
-        output = (1, 0)
     if dominant_color == b :
         print("temp_blue")
-        colors['blue'] = colors.get('blue') + 1
+    
+    elif dominant_color == r :
+        print("temp_red")
+        
+    elif dominant_color == g :
+        print("temp_golden")
+        
+        
 
     cv2.imshow("frame", frame)
     number_of_frames -= 1
-    if cv2.waitKey(1) == 27:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-which_color = colors.keys()[colors.values().index(max(colors.values()))]
-if which_color == 'red':
-    rpi_output(3, 5,7)
-elif which_color == 'blue':
-    rpi_output(5, 3,7)
-else :
-    rpi_output(7, 3,5)
 cv2.destroyAllWindows()
 cap.release()
